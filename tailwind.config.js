307f9cbcfb6763607c83098291e1c7436110745b
I-AM-Poorya <p.aliabadia81@gmail.com>
2024-08-10 15:02:13 -0700

    'forTest'

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./Public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary" : {
+          DEFAULT : "#8E44AD",
+          100: "#E67E22" ,
+          200: "#2ECC71",
+          "lighter" : "#F1C40F"
+        },
+      },
+    },
   },
   plugins: [],
 }
